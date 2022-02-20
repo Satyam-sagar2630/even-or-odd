50f6bb160a6a3848d57d4152506bf06b741e9fee
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-20 23:46:19 +0530

    Add files via upload

---
+++ even or odd/even or odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+          
+            Console.WriteLine("Enter your number :");
+            int n=Convert.ToInt32(Console.ReadLine());
+            if(n%2==0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+            }
+                }
+    }
+}
